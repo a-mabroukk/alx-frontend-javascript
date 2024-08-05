a6bf61e6b65762e26d45aae647fd6f2ae7a16f24
Alaa Fayed <alaafayed810@gmail.com>
2024-08-06 00:10:49 +0300

    all

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -3,4 +3,4 @@ export default function createReportObject(employeesList) {
       allEmployees: { ...employeesList },
       getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
     };
-  }
+}
--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,7 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-    expansion1989 = expansion1989 === undefined ? "Default User" : expansion1989
-    
-    expansion2019 = expansion2019 === undefined ? "Default User" : expansion2019
-
+export default function getSumOfHoods(
+    initialNumber,
+    expansion1989 = 89,
+    expansion2019 = 19
+) {
     return initialNumber + expansion1989 + expansion2019;
-  }
+}
