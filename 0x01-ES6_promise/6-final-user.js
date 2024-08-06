ea821237e6d6d8f41fcf3f8ebd68f32c4f419901
Alaa Fayed <alaafayed810@gmail.com>
2024-08-07 00:21:29 +0300

    all

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-  return await Promise.allSettled(promises).then((results) => {
+  return Promise.allSettled(promises).then((results) => {
     const finalResults = []; // Renamed here
     results.map(({ status, value, reason }) => (
       finalResults.push({ // Using finalResults instead of results
