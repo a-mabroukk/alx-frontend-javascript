63a0b8d5bd9627f82d4d0bbeacc0ae70533994ca
Alaa Fayed <alaafayed810@gmail.com>
2024-08-08 16:40:29 +0300

    2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,34 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(val) {
+        if (typeof(val) !== "string") {
+            throw new TypeError("Name must be a string");
+        }
+        this._name = val;
+    }
+    get lenght() {
+        return this._length;
+    }
+    set lenght(val) {
+        if (typeof(val) !== "number") {
+            throw new TypeError("Length must be a number");
+        }
+        this._length = val;
+    }
+    get students() {
+        return this._students;
+    }
+    set students(val) {
+        if (!Array.isArray(val)) {
+            throw new TypeError("Students must be an array of strings");
+        }
+        this._students = val;
+    }
+}
