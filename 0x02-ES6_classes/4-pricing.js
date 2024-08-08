7c4a8b1ebdc7ccd9b66bd61371c1c99a5c0b1498
Alaa Fayed <alaafayed810@gmail.com>
2024-08-08 17:30:53 +0300

    4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,28 @@
+import Currency from "./3-currency";
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
+    get amount() {
+        return this._amount;
+    }
+    set amount(val) {
+        if (typeof(val) !== "number") {
+            throw new TypeError("amount must be a number");
+        }
+        this._amount = val;
+    }
+    get currency() {
+        return this._currency;
+    }
+    set currency(val) {
+        this._currency = val;
+    }
+    displayFullPrice() {
+        return `${this.amount} ${this.currency.name} ${this._currency._code}`;
+    }
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
